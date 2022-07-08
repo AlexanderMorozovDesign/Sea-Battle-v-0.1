66ee3994f83d63f4e0bac984dc33bcb5b183a8fe
AlexM <AlexM@DESKTOP-C8C80H1>
2022-07-08 19:13:40 +0700

    edit and add code

--- Sea Battle v 0.1/Form1.cs
+++ Sea Battle v 0.1/Form1.cs
@@ -12,10 +12,107 @@ namespace Sea_Battle_v_0._1
 {
     public partial class Form1 : Form
     {
+        public const int mapSize = 10;
+        public int cellSize = 30;
+        public string alphabet = "ABCDEFGHIJ";
+
+        public int[,] myMap = new int[mapSize, mapSize];
+        public int[,] enemyMap = new int[mapSize, mapSize];
+
+        public bool isPlaying = false;
+
         public Form1()
         {
             InitializeComponent();
+            this.Text = "Classic Sea Battle v 0.1";
+            Init();
         }
 
+        public void Init()
+        {
+            isPlaying = false;
+            CreateMaps();
+        }
+
+        public void CreateMaps()
+        {
+            this.Width = mapSize * 2 * cellSize + 50;
+            this.Height = (mapSize + 3) * (cellSize + 20);
+
+            for (int i = 0; i < mapSize; i++)
+            {
+                for (int j = 0; j < mapSize; j++)
+                {
+                    myMap[i, j] = 0;
+
+                    Button button = new Button();
+                    button.Location = new Point(j * cellSize, i * cellSize);
+                    button.Size = new Size(cellSize, cellSize);
+                    if (j == 0 || i == 0)
+                    {
+                        button.BackColor = Color.Gray;
+                        if (i == 0 && j > 0)
+                        {
+                            button.Text = alphabet[j - 1].ToString();
+                        }
+                        if (j == 0 && i > 0)
+                        {
+                            button.Text = i.ToString();
+                        }
+
+                    }
+
+                    this.Controls.Add(button);
+                }
+            }
+            for (int i = 0; i < mapSize; i++)
+            {
+                for (int j = 0; j < mapSize; j++)
+                {
+                    myMap[i, j] = 0;
+                    enemyMap[i, j] = 0;
+
+                    Button button = new Button();
+                    button.Location = new Point(320 + j * cellSize, i * cellSize);
+                    button.Size = new Size(cellSize, cellSize);
+                    if (j == 0 || i == 0)
+                    {
+                        button.BackColor = Color.Gray;
+                        if (i == 0 && j > 0)
+                        {
+                            button.Text = alphabet[j - 1].ToString();
+                        }
+                        if (j == 0 && i > 0)
+                        {
+                            button.Text = i.ToString();
+                        }
+
+                    }
+
+                    this.Controls.Add(button);
+                }
+            }
+
+            Label map1 = new Label();
+            map1.Text = "Карта Игрока";
+            map1.Location = new Point(mapSize * cellSize / 2, mapSize * cellSize + 10);
+            this.Controls.Add(map1);
+
+            Label map2 = new Label();
+            map2.Text = "КартаПротивника";
+            map2.Location = new Point(250+mapSize * cellSize / 2, mapSize * cellSize + 10);
+            this.Controls.Add(map2);
+
+            Button startButton = new Button();
+            startButton.Text = "Start Game";
+            startButton.Click += new EventHandler(Start);
+            startButton.Location = new Point(0, mapSize * cellSize + 20);
+            this.Controls.Add(startButton);
+        }
+
+        public void Start(object sender, EventArgs e)
+        {
+            isPlaying = true;
+        }
     }
 }
