22e9200660d8784144aae742c58a12a19ca9f33e
AlexM <AlexM@DESKTOP-IVUST5O>
2022-07-10 15:23:35 +0700

    add Bot edit form

--- Sea Battle v 0.1/Form1.cs
+++ Sea Battle v 0.1/Form1.cs
@@ -14,17 +14,22 @@ namespace Sea_Battle_v_0._1
     {
         public const int mapSize = 10;
         public int cellSize = 30;
-        public string alphabet = "ABCDEFGHIJ";
+        public string alphabet = "АБВГДЕЖЗИК";
 
         public int[,] myMap = new int[mapSize, mapSize];
         public int[,] enemyMap = new int[mapSize, mapSize];
 
+        public Button[,] myButtons = new Button[mapSize, mapSize];
+        public Button[,] enemyButtons = new Button[mapSize, mapSize];
+
         public bool isPlaying = false;
+
+        public Bot bot;
 
         public Form1()
         {
             InitializeComponent();
-            this.Text = "Classic Sea Battle v 0.1";
+            this.Text = "Морской бой";
             Init();
         }
 
@@ -32,13 +37,14 @@ namespace Sea_Battle_v_0._1
         {
             isPlaying = false;
             CreateMaps();
+            bot = new Bot(enemyMap, myMap, enemyButtons, myButtons);
+            enemyMap = bot.ConfigureShips();
         }
 
         public void CreateMaps()
         {
             this.Width = mapSize * 2 * cellSize + 50;
-            this.Height = (mapSize + 3) * (cellSize + 20);
-
+            this.Height = (mapSize + 3) * cellSize + 20;
             for (int i = 0; i < mapSize; i++)
             {
                 for (int j = 0; j < mapSize; j++)
@@ -48,25 +54,20 @@ namespace Sea_Battle_v_0._1
                     Button button = new Button();
                     button.Location = new Point(j * cellSize, i * cellSize);
                     button.Size = new Size(cellSize, cellSize);
-                    
+                    button.BackColor = Color.White;
                     if (j == 0 || i == 0)
                     {
                         button.BackColor = Color.Gray;
                         if (i == 0 && j > 0)
-                        {
                             button.Text = alphabet[j - 1].ToString();
-                        }
                         if (j == 0 && i > 0)
-                        {
                             button.Text = i.ToString();
-                        }
-
                     }
                     else
                     {
                         button.Click += new EventHandler(ConfigureShips);
                     }
-
+                    myButtons[i, j] = button;
                     this.Controls.Add(button);
                 }
             }
@@ -80,36 +81,35 @@ namespace Sea_Battle_v_0._1
                     Button button = new Button();
                     button.Location = new Point(320 + j * cellSize, i * cellSize);
                     button.Size = new Size(cellSize, cellSize);
+                    button.BackColor = Color.White;
                     if (j == 0 || i == 0)
                     {
                         button.BackColor = Color.Gray;
                         if (i == 0 && j > 0)
-                        {
                             button.Text = alphabet[j - 1].ToString();
-                        }
                         if (j == 0 && i > 0)
-                        {
                             button.Text = i.ToString();
-                        }
-
                     }
-
+                    else
+                    {
+                        button.Click += new EventHandler(PlayerShoot);
+                    }
+                    enemyButtons[i, j] = button;
                     this.Controls.Add(button);
                 }
             }
-
             Label map1 = new Label();
-            map1.Text = "Карта Игрока";
+            map1.Text = "Карта игрока";
             map1.Location = new Point(mapSize * cellSize / 2, mapSize * cellSize + 10);
             this.Controls.Add(map1);
 
             Label map2 = new Label();
-            map2.Text = "Карта Противника";
-            map2.Location = new Point(250+mapSize * cellSize / 2, mapSize * cellSize + 10);
+            map2.Text = "Карта противника";
+            map2.Location = new Point(350 + mapSize * cellSize / 2, mapSize * cellSize + 10);
             this.Controls.Add(map2);
 
             Button startButton = new Button();
-            startButton.Text = "Start Game";
+            startButton.Text = "Начать";
             startButton.Click += new EventHandler(Start);
             startButton.Location = new Point(0, mapSize * cellSize + 20);
             this.Controls.Add(startButton);
@@ -118,6 +118,26 @@ namespace Sea_Battle_v_0._1
         public void Start(object sender, EventArgs e)
         {
             isPlaying = true;
+
+        }
+
+        public bool CheckIfMapIsNotEmpty()
+        {
+            bool isEmpty1 = true;
+            bool isEmpty2 = true;
+            for (int i = 1; i < mapSize; i++)
+            {
+                for (int j = 1; j < mapSize; j++)
+                {
+                    if (myMap[i, j] != 0)
+                        isEmpty1 = false;
+                    if (enemyMap[i, j] != 0)
+                        isEmpty2 = false;
+                }
+            }
+            if (isEmpty1 || isEmpty2)
+                return false;
+            else return true;
         }
 
         public void ConfigureShips(object sender, EventArgs e)
@@ -125,11 +145,57 @@ namespace Sea_Battle_v_0._1
             Button pressedButton = sender as Button;
             if (!isPlaying)
             {
-                pressedButton.BackColor = Color.Red;
-
+                if (myMap[pressedButton.Location.Y / cellSize, pressedButton.Location.X / cellSize] == 0)
+                {
+                    pressedButton.BackColor = Color.Red;
+                    myMap[pressedButton.Location.Y / cellSize, pressedButton.Location.X / cellSize] = 1;
+                }
+                else
+                {
+                    pressedButton.BackColor = Color.White;
+                    myMap[pressedButton.Location.Y / cellSize, pressedButton.Location.X / cellSize] = 0;
+                }
             }
         }
 
-         // 7 42
+        public void PlayerShoot(object sender, EventArgs e)
+        {
+
+            Button pressedButton = sender as Button;
+            bool playerTurn = Shoot(enemyMap, pressedButton);
+            if (!playerTurn)
+                bot.Shoot();
+
+            if (!CheckIfMapIsNotEmpty())
+            {
+                this.Controls.Clear();
+                Init();
+            }
+        }
+
+        public bool Shoot(int[,] map, Button pressedButton)
+        {
+            bool hit = false;
+            if (isPlaying)
+            {
+                int delta = 0;
+                if (pressedButton.Location.X > 320)
+                    delta = 320;
+                if (map[pressedButton.Location.Y / cellSize, (pressedButton.Location.X - delta) / cellSize] != 0)
+                {
+                    hit = true;
+                    map[pressedButton.Location.Y / cellSize, (pressedButton.Location.X - delta) / cellSize] = 0;
+                    pressedButton.BackColor = Color.Blue;
+                    pressedButton.Text = "X";
+                }
+                else
+                {
+                    hit = false;
+
+                    pressedButton.BackColor = Color.Black;
+                }
+            }
+            return hit;
+        }
     }
 }
